eb09e8f274421bab4d3ec52f328f09cd89c7a584
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-02-28 22:50:09 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { TbBrandGithub, TbBrandInstagram, TbBrandLinkedin } from 'react-icons/tb
 function Footer() {
   return (
     <div className='w-full h-auto py-10 flex font-[oswald] bg-neutral-950 px-5'>
-      <div className='w-1/2 uppercase text-2xl flex items-center justify-start sm:text-4xl font-bold text-neutral-100'><span>SachinXfolio</span></div>
+      <div className='w-1/2 uppercase text-2xl flex items-center justify-start sm:text-4xl font-bold text-neutral-100'><span>SainXFolio</span></div>
       <div className='w-1/2 gap-2 sm:gap-5 text-3xl sm:text-4xl flex items-center justify-end px-5 sm:px-10'>
         <span className='p-2 rounded-full bg-neutral-100'><a href="https://www.instagram.com/ig.sain.dev/" target='_blank'><TbBrandInstagram /></a></span>
         <span className='p-2 rounded-full bg-neutral-100'><a href="https://www.linkedin.com/in/sachin-pal11/" target='_blank'><TbBrandLinkedin /></a></span>
